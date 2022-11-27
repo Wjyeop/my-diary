1890ff0bdd552cd1c6b840532527ba506e07a575
Wjyeop <wnsduq2178@gmail.com>
2022-11-28 01:04:23 +0900

    페이지설정

---
+++ src/pages/home/Home.js
@@ -0,0 +1,24 @@
+import styles from './Home.module.css'
+import DairyForm from './DiaryForm'
+import DiaryList from './DiaryList';
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useCollection } from '../../hooks/useCollection';
+
+export default function Home() {
+
+    const { user } = useAuthContext();
+    const { documents, error } = useCollection('diary', ["uid", "==", user.uid ]);
+
+    return (
+        <main className={styles.cont}>
+            <aside className={styles.side_menu}>
+                <DairyForm uid={user.uid}></DairyForm>
+            </aside>
+            <ul className={styles.content_list}>
+                {error && <strong>{error}</strong>}
+                {documents && <DiaryList diaries={documents} />}
+            </ul>
+        </main>
+        
+    )
+}
