f5822931f13dbd0e6bd323cd5bcab0dae0adfd72
Wjyeop <wnsduq2178@gmail.com>
2022-11-28 01:04:45 +0900

    hook 설정

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,65 @@
+import { addDoc, collection, deleteDoc, doc } from "firebase/firestore"
+import { useReducer } from "react"
+import { appFireStore, timestamp } from "../firebase/config"
+
+
+const initState = {
+    document : null,
+    isPending: false,
+    error: null,
+    success: false
+}
+
+const storeReducer = (state, action) => {
+    switch (action.type) {
+        case 'isPending' :
+            return { isPending: true, document: null, success: false, error: null } 
+        case 'addDoc' :
+            return { isPending: false, document: action.payload, success: true, error: null } 
+        case 'error' :
+            return { isPending: false, document: null, success: false, error: action.payload } 
+        case 'deleteDoc':
+            return { isPending: false, document: action.payload, success: true, error: null } 
+        default:
+            return state
+    }
+}
+
+// 저장할 컬렉션을 인자로 전달
+export const useFirestore = (transaction) => {
+    
+    const [response, dispatch] = useReducer(storeReducer, initState);
+
+    // colRef : 컬렉션의 참조를 요구
+    const colRef = collection(appFireStore, transaction);
+
+    //컬렉션에 문서를 추가한다.
+    const addDocument = async (doc) => {
+        dispatch({ type:"isPending" });
+
+        try {
+            const createdTime = timestamp.fromDate(new Date());
+            const docRef = await addDoc(colRef, { ...doc, createdTime });
+            console.log(docRef)
+            dispatch({ type:"addDoc", payload: docRef });
+        }catch (error) {    
+            dispatch({ type:"error", payload: error.message });
+        }
+
+    }
+
+    // 컬렉션에 문서를 제거
+    const deleteDocument = async (id) => {
+            dispatch({ type:"isPending" });
+    
+            try {
+                const docRef = await deleteDoc(doc(colRef, id));
+                dispatch({ type:"addDoc", payload: docRef });
+            }catch (error) {    
+                dispatch({ type:"error", payload: error.message });
+            }
+    
+    }    
+
+    return {addDocument,deleteDocument,response}
+}
---
+++ src/hooks/useSignup.js
@@ -0,0 +1,44 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { appAuth } from '../firebase/config'
+import { useState } from "react"
+import { useAuthContext } from "./useAuthContext";
+
+export const useSignup = () => {
+    // 에러 정보를 저장합니다.
+    const [error, setError] = useState(null);
+    // 현재 서버와 통신 상태를 저장합니다.
+    const [isPending, setIsPending] = useState(false);
+    const { dispatch } = useAuthContext();
+    
+    const signup = (email, password, displayName) => {
+        setError(null); // 아직 에러가 없습니다.
+        setIsPending(true); // 통신을 진행 중입니다.
+
+        createUserWithEmailAndPassword(appAuth, email, password)
+            .then((useCredential) => {
+                const user = useCredential.user;
+                console.log(user);
+
+                if(!user){
+                    throw new Error('회원가입에 실패했습니다.')
+                }
+
+                updateProfile(appAuth.currentUser, { displayName })
+                .then(() => {
+                    dispatch({ type : 'login', payload: user });
+                    setError(null);
+                    setIsPending(false);
+                }).catch((err) => {
+                    setError(err.message);
+                    setIsPending(false);
+                    console.log(err.message);
+                })
+
+            }).catch((err) => {
+                setError(err.message);
+                setIsPending(false);
+                console.log(err.message);
+            })
+    }
+    return { error, isPending, signup }
+}
