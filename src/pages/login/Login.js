1d3ad18185b025c5e1bc73dbec6f57db2d3bd123
Wjyeop <wnsduq2178@gmail.com>
2023-06-16 00:47:59 +0900

    로그인 폼 제작 및 CSS 설정

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,38 @@
+import { useState } from 'react'
 import styles from './Login.module.css'
 
 export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleData = (e) => {
+        if(e.target.type === "email") {
+            setEmail(e.target.value); 
+        } else if (e.target.type === "password") {
+            setPassword(e.target.value);
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email,password);
+    }
+
     return (
-        <div>Login</div>
+        <form className={styles.login_form} onSubmit={handleSubmit}>
+            <fieldset>
+                <legend>로그인</legend>
+                <label htmlFor='myEmail'>email : </label>
+                <input type="email" id="myEmail" required value={email} onChange=
+                {handleData}/>
+
+                <label htmlFor='myPassword'>password : </label>
+                <input type="password" id="myPassword" required value={password} onChange=
+                {handleData}/>
+
+                <button type='submit' className={styles.btn}>로그인</button>
+            </fieldset>
+
+        </form>
     )
 }
