5b793437eb5c466c21c982960a4c19b250cfbd26
Wjyeop <wnsduq2178@gmail.com>
2023-06-17 16:16:35 +0900

    되돌리기

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC2-Gb9MUHQJlvwf5RFhhHsbSo3xDgMQdE",
-  authDomain: "dailyapp-679aa.firebaseapp.com",
-  databaseURL: "https://dailyapp-679aa-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "dailyapp-679aa",
-  storageBucket: "dailyapp-679aa.appspot.com",
-  messagingSenderId: "31180916772",
-  appId: "1:31180916772:web:e6a0b9b202209970a0b0b1",
-  measurementId: "G-6H80Z2JYD8"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
