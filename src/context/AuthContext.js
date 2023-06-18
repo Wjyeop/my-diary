0449c0f4ffde11454bd4e6c025b6cd64f8e01d50
Wjyeop <wnsduq2178@gmail.com>
2023-06-18 15:08:21 +0900

    로그인 정보 페이지  분기 처리

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,7 @@
-import { createContext, useReducer } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect, useReducer } from "react";
+import { appAuth } from "../firebase/config";
 
-//
 const AuthContext = createContext();
 
 const authReducer = (state, action) => {
@@ -9,16 +10,28 @@ const authReducer = (state, action) => {
             return { ...state, user: action.payload }
         case 'logout':
             return { ...state, user: null }
+        case 'isAuthReady':
+            return { ...state, user: action.payload, isAuthReady: true }
         default:
             return state
     }
 }
 
+
+
 const AuthContextProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(authReducer, {
-        user: null
+        user: null,
+        isAuthReady: false
     })
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(appAuth, (user) => {
+            dispatch({type:'isAuthReady', payload: user});
+        })
+        return unsubscribe;
+    }, [])
 
     console.log('user state : ', state)
     return (
